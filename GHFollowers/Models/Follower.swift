613964265680d6340c60e8c9159be620fa79b367
Ian Brault <ian@brault.dev>
2023-03-10 14:42:51 -0800

    set up models and bugfix navigation controllers

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Ian Brault on 3/10/23.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
